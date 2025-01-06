7b01b199b0874b13b819aa75055adcf894fa41a5
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-06 21:33:19 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,1 +1,18 @@
+ESX.RegisterServerCallback('esx_fishing:addFish', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.addInventoryItem('fish', 1)
+end)
 
+ESX.RegisterServerCallback('esx_fishing:sellFish', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local fishCount = xPlayer.getInventoryItem('fish').count
+
+    if fishCount > 0 then
+        local payment = fishCount * 50 -- $50 par poisson
+        xPlayer.removeInventoryItem('fish', fishCount)
+        xPlayer.addMoney(payment)
+        TriggerClientEvent('esx:showNotification', source, "Vous avez vendu " .. fishCount .. " poissons pour $" .. payment)
+    else
+        TriggerClientEvent('esx:showNotification', source, "Vous n'avez pas de poissons à vendre.")
+    end
+end)
